9f4dd816c89d834c98a17bae495c0bb8af3fabed
richard3983 <richard3983@gmail.com>
2021-05-26 22:33:00 +0800

    fix xai error

--- explanation_utils.py
+++ explanation_utils.py
@@ -100,8 +100,8 @@ def explanation_hook(module, grad_input, grad_output):
     :return:
     """
     # get stored mask
-    temp = images_to_vectors(get_values())
-
+    # temp = images_to_vectors(get_values())
+    temp = get_values()
     # multiply with mask to generate values in range [1x, 1.2x] where x is the original calculated gradient
     # can be tuned!!!!!!!!!!!!!******************
     new_grad = grad_input[0] + 0.2 * (grad_input[0] * temp)
